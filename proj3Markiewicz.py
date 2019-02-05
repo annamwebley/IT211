c849beb33a856fbdda18d75127e39e09f0b755c2
annamwebley <35773238+annamwebley@users.noreply.github.com>
2019-02-05 17:32:44 -0600

    Update proj3Markiewicz.py

--- proj3Markiewicz.py
+++ proj3Markiewicz.py
@@ -6,11 +6,14 @@
 # Fields in grade-records.txt input file:
 # stud_id, last_name, first_name, course_number, course_name, prof_name, credit_hours, grade
 
-desired_first_name = input('What is your first name: ')
-desired_last_name = input('What is your last name: ')
+#desired_first_name = input('What is your first name: ')
+#desired_last_name = input('What is your last name: ')
 
 total_credit_hours = 0
 total_grade_points = 0
+
+desired_first_name = "Raymond"
+desired_last_name = "Hoffman"
 
 # Opening text file that will read the first line
 
@@ -26,7 +29,6 @@ line = fin.readline( )
 
 # Begin while statement when the line is not empty
 while line != "":
-	grade_points = 0
 	fields = line.split(",")
 	last_name = fields[1].strip( )
 	first_name = fields[2].strip( )
@@ -34,7 +36,25 @@ while line != "":
 	grade = fields[7].strip('\n')
 	if first_name == desired_first_name and last_name == desired_last_name:
 		if grade == "A":
-			credit_hours = grade_points
+			grade_points = credit_hours * 4
+		elif grade == "B":
+			grade_points = credit_hours * 3
+		elif grade == "C":
+			grade_points = credit_hours * 2
+		elif grade == "D":
+			grade_points = credit_hours * 1
+		else:
+			grade_points = credit_hours * 0
+
+		# End of if ... else statements 
+	else:
+		print("Please enter correct name")
+		# End of if statement
+
+	total_credit_hours += credit_hours
+else:
+	pass
+
 
 	print(f"last_name={last_name}, first_name={first_name}, credit_hours={credit_hours}, grade={grade}")
 	print(fields)
